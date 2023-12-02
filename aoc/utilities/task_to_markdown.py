9fce63c7683f1dead9bdcbee0be81c2605da18b3
Ján Popeláš <janopopelas@gmail.com>
2023-12-02 23:55:38 +0100

    feat: add conversion to markdown

---
+++ aoc/utilities/task_to_markdown.py
@@ -0,0 +1,29 @@
+# pylint: skip-file
+
+import markdownify
+
+
+class AdventOfCodeHtmlToMarkdown:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.content: str = ""
+        self._load_content()
+
+    def _load_content(self) -> None:
+        with open(self.file_path, "r") as file:
+            self.content = file.read().strip()
+
+    @property
+    def markdown(self) -> str:
+        return markdownify.markdownify(
+            self.content, heading_style="ATX", strip=["script", "style", "link"]
+        )
+
+
+def main():
+    converter = AdventOfCodeHtmlToMarkdown("day.html")
+    print(converter.markdown)
+
+
+if __name__ == "__main__":
+    main()
